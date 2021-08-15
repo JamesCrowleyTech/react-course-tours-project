f77ea5733cecf74d34c72adda6b5e965c3a66556
JamesCrowleyTech <jamescrowley467@gmail.com>
2021-08-15 20:46:30 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { API_URL } from "./config";
+
+(async function () {
+    const data = await fetch(API_URL);
+    const s = await data.json();
+})();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
